709d0014fab5d4c6048cbe400c043a8b68d90ec6
Daniil Rybalko <d.rybalko@smartengines.com>
2022-04-12 18:47:13 +0300

    added processed pictures

--- process_pictures.py
+++ process_pictures.py
@@ -28,12 +28,22 @@ def process_picture(image_path, config):
     #proc = subprocess.Popen('cmd.exe', stdin = subprocess.PIPE, stdout = subprocess.PIPE, encoding='utf8')
     command = form_command(image_path, config)
     compl_process = subprocess.run(command.split(' '), capture_output=True)
-    path_to_reject_data = ""
-    doctype = "" #get doctype from stdout or stderr or file
-    rejected_data = False #process_reject_data(path_to_reject_data)
     temp = compl_process.stdout.decode('utf8')
-    print(temp)
-    return doctype, rejected_data
+    temp_json = json.loads(temp)
+    doctype = temp_json["type"]
+    conf = 0.0
+    is_rejected = True
+    for key in temp_json["match"]["templates"]:
+        try:
+            conf = temp_json["match"]["templates"][key]["confidence"]
+            is_rejected = False if temp_json["match"]["templates"][key]["is_rejected"] == False else True
+            processed = True
+            break
+        except:
+            continue
+    if conf == 0.0000:
+        processed = False
+    return doctype, conf, is_rejected, processed
 
 
 def process_sample(df, exe_path, bundle_path):
@@ -42,19 +52,28 @@ def process_sample(df, exe_path, bundle_path):
     doctype = []
     guessed_doctype = []
     rejected = []
+    confidence = []
+    is_processed = []
+    total_unprocessed = 0
     i = 0
-    for image in df["Image path"]:
+    for image in tqdm.tqdm(df["Image path"]):
         img_path.append(image)
-        g_doctype, is_rejected = process_picture(image, config)
+        g_doctype, g_confidence, is_rejected, processed = process_picture(image, config)
+        if processed == False:
+            total_unprocessed += 1
+            is_processed.append(False)
+        else:
+            is_processed.append(True)
         guessed_doctype.append(g_doctype)
         rejected.append(is_rejected)
+        confidence.append(g_confidence)
         doctype.append(df["Document Type"][i])
         i+=1
     res = pd.DataFrame({"Image path": img_path, "Document Type": doctype, \
-        "Processed Document Type": guessed_doctype, "Is rejected": rejected})
-    return res
+        "Processed Document Type": guessed_doctype, "Is rejected": rejected, "Confidence" : confidence, "Is processed" : is_processed})
+    return res, total_unprocessed
 
 def process_result(res):
-    #TODO
+    #build roc curve
     return 0
 
