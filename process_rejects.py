2f6d9532702a4413ad678e6df4668e15d64960bc
Daniil Rybalko <d.rybalko@smartengines.com>
2022-04-27 13:45:37 +0300

    added roc curve

--- process_rejects.py
+++ process_rejects.py
@@ -14,10 +14,47 @@ doctypes_vocab = {"alb.id.type1" : "alb_id",
                     "" : "" } #debug
 
 def match_doctypes(result, i):
-    if result["Document Type"][i] == doctypes_vocab[result["Processed Document Type"][i]]:
-        return True
-    else:
+    try:
+        if result["Document Type"][i] == doctypes_vocab[result["Processed Document Type"][i].split(":")[0]] and result["Document Type"][i] != "":
+            return True
+        else:
+            return False
+    except:
         return False
+
+def conf_score(num_inliers):
+    if num_inliers != 0:
+        return (max(0, num_inliers - 4))/num_inliers #TODO
+    else:
+        return 0.0
+
+def process_rejected_file(df, file):
+    with open(file, encoding="utf8") as f:
+        lines = [line.strip() for line in f.readlines()]
+    index = 0
+    max_inliers = 0
+    max_tpl = ""
+    for line in lines:
+        if line != "------":
+            if int(line.strip().split(' | ')[0]) > max_inliers:
+                max_inliers = int(line.strip().split(' | ')[0])
+                max_tpl = line.strip().split(' | ')[1]
+        else:
+            df["Processed Document Type"][index] = max_tpl.split(":")[0]
+            df["Confidence"][index] = conf_score(max_inliers)
+            max_inliers = 0
+            max_tpl = ""
+            index += 1
+    return df
+
+
+def fix_df(unproc_df, df):
+    counter = 0
+    for line in unproc_df["Unnamed: 0"]:
+        df["Processed Document Type"][line] = unproc_df["Processed Document Type"][counter]
+        df["Confidence"][line] = unproc_df["Confidence"][counter]
+        counter += 1
+    return df
 
 def get_tpr(result, thresh):
     total_p = 0
@@ -27,7 +64,10 @@ def get_tpr(result, thresh):
             total_p += 1
             if result["Confidence"][i] < thresh:
                 total_tp += 1
-    return total_tp / total_p
+    if total_p != 0:
+        return total_tp / total_p
+    else:
+        return 0.0
 
 
 def get_fpr(result, thresh):
@@ -38,10 +78,13 @@ def get_fpr(result, thresh):
             total_n += 1
             if result["Confidence"][i] < thresh:
                 total_fp += 1
-    return total_fp / total_n
+    if total_n != 0:
+        return total_fp / total_n
+    else:
+        return 0.0
 
 def get_roc_curve(data, margin):
     points = []
     for i in range(0, 100, int(margin*100)):
-        points.append([get_tpr(data, i/100), get_fpr(data, i/100)])
+        points.append([get_fpr(data, i/100), get_tpr(data, i/100)])
     return points
