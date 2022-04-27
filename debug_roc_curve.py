9575b190d0a8838f69555ca84082facc44696f17
Daniil Rybalko <d.rybalko@smartengines.com>
2022-04-27 18:01:47 +0300

    added file for debugging roc curve

---
+++ debug_roc_curve.py
@@ -0,0 +1,13 @@
+import random
+import pandas as pd
+
+def form_random_sample(df, num=50):
+    right_doc_type = []
+    wrong_doc_type = []
+    right_ = []
+    wrong_ = []
+    #TODO ...
+    for i in range(len(df["Image path"])):
+        if df["Is rejected"][i] == "False":
+            right.append
+    return 0
