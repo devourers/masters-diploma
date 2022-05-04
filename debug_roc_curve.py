4be47c001edb9b1e4389188c2b5e6ee8d92355f3
Daniil Rybalko <d.rybalko@smartengines.com>
2022-05-04 11:01:03 +0300

    debugged roc curve

--- debug_roc_curve.py
+++ debug_roc_curve.py
@@ -1,13 +1,22 @@
 import random
 import pandas as pd
+from torch import rand
 
-def form_random_sample(df, num=50):
+def form_random_sample(df):
+    image_path = []
+    is_rejected = []
+    is_processed = []
     right_doc_type = []
-    wrong_doc_type = []
-    right_ = []
-    wrong_ = []
-    #TODO ...
+    processed_doc_type = []
+    conf = []
     for i in range(len(df["Image path"])):
-        if df["Is rejected"][i] == "False":
-            right.append
-    return 0
+        image_path.append(df["Image path"][i])
+        right_doc_type.append(df["Document Type"][i])
+        processed_doc_type.append(df["Processed Document Type"][i])
+        is_rejected.append(df["Is rejected"][i])
+        is_processed.append(df["Is processed"][i])
+        conf.append(random.randint(0, 100) / 100)
+    res_df = pd.DataFrame({"Image path" : image_path, "Document Type" : right_doc_type,\
+         "Processed Document Type" : processed_doc_type, "Is rejected" : is_rejected, \
+         "Confidence" : conf, "Is processed" : is_processed})
+    return res_df
